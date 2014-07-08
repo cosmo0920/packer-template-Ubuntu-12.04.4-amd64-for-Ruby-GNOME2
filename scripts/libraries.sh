3e4dcdf57405e6e81b05cd6f01336f8391d9e993
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-07-09 01:36:47 +0900

    modify provision script

--- scripts/libraries.sh
+++ scripts/libraries.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 sudo apt-get install -y software-properties-common python-software-properties
-sudo add-apt-repository --yes ppa:gstreamer-developers/ppa
+
+if ! apt-cache show gir1.2-gstreamer-1.0 > /dev/null 2>&1; then
+    sudo add-apt-repository --yes ppa:gstreamer-developers/ppa
+fi
 sudo apt-get update -qq
-sudo apt-get install -qq -y libgirepository1.0-dev libgstreamer1.0-plugins-good gir1.2-clutter-1.0 gir1.2-clutter-gst-1.0 gir1.2-gtkclutter-1.0 hicolor-icon-theme gnome-icon-theme
+sudo apt-get install -qq -y \
+    libgirepository1.0-dev \
+    gstreamer1.0-plugins-good \
+    gir1.2-clutter-1.0 \
+    gir1.2-clutter-gst-1.0 \
+    gir1.2-gtkclutter-1.0 \
+    hicolor-icon-theme \
+    gnome-icon-theme
